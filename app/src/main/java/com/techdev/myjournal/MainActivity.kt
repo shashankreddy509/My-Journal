8dcc47d9ab87fed6b39d9c40b58122ade0baf157
Shashank Reddy <shashankreddy509@gmail.com>
2023-04-10 21:32:37 +0530

    Removed un wanted code. (#2)
    * Removed un wanted code.
    
    * Fixed code lint

--- app/src/main/java/com/techdev/myjournal/MainActivity.kt
+++ app/src/main/java/com/techdev/myjournal/MainActivity.kt
@@ -3,7 +3,6 @@ package com.techdev.myjournal
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.onegravity.rteditor.RTEditText
 import com.onegravity.rteditor.RTManager
@@ -37,9 +36,6 @@ class MainActivity : AppCompatActivity() {
         rtEditText = findViewById<View>(R.id.rteMain) as RTEditText
         rtManager.registerEditor(rtEditText, true)
         rtEditText.setRichTextEditing(true, "")
-
-        findViewById<Button>(R.id.btnNextActivity).setOnClickListener {
-        }
     }
 
     override fun onDestroy() {
